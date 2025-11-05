9545122795d489979b434381d1f3c5c38910a705
dreamfarer <dreamfarer@pm.me>
2025-11-05 18:04:22 +0100

    chore: remove parts coded in class

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,6 +1,5 @@
 extends Node
 
-@export var pipe_scene : PackedScene
 @export var scroll_speed : int = 2
 
 var game_running : bool
@@ -8,9 +7,6 @@ var game_over : bool
 var scroll
 var score
 var ground_height : int
-var pipes : Array
-const PIPE_DELAY : int = 100
-const PIPE_RANGE : int = 200
 
 func _ready():
 	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
@@ -23,27 +19,15 @@ func new_game():
 	scroll = 0
 	$ScoreLabel.text = "SCORE: " + str(score)
 	$GameOver.hide()
-	get_tree().call_group("pipes", "queue_free")
-	pipes.clear()
-	generate_pipes()
 	$Bird.reset()
 	
-func _input(event):
-	if game_over == false:
-		if event is InputEventMouseButton:
-			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-				if game_running == false:
-					start_game()
-				else:
-					if $Bird.flying:
-						$Bird.flap()
-						check_top()
+func _input(_event):
+	pass
 
 func start_game():
 	game_running = true
 	$Bird.flying = true
 	$Bird.flap()
-	$PipeTimer.start()
 
 func _process(_delta):
 	if game_running:
@@ -51,40 +35,12 @@ func _process(_delta):
 		if scroll >= get_window().size.x:
 			scroll = 0
 		$Ground.position.x = -scroll
-		for pipe in pipes:
-			pipe.position.x -= scroll_speed
-
-func generate_pipes():
-	var pipe = pipe_scene.instantiate()
-	pipe.position.x = get_window().size.x + PIPE_DELAY
-	pipe.position.y = (get_window().size.y - ground_height) / 2  + randi_range(-PIPE_RANGE, PIPE_RANGE)
-	pipe.hit.connect(bird_hit)
-	pipe.scored.connect(scored)
-	add_child(pipe)
-	pipes.append(pipe)
-	
-func scored():
-	score += 1
-	$ScoreLabel.text = "SCORE: " + str(score)
-
-func check_top():
-	if $Bird.position.y < 0:
-		$Bird.falling = true
-		stop_game()
 
 func stop_game():
-	$PipeTimer.stop()
 	$GameOver.show()
 	$Bird.flying = false
 	game_running = false
 	game_over = true
-	
-func bird_hit():
-	$Bird.falling = true
-	stop_game()
-
-func _on_pipe_timer_timeout():
-	generate_pipes()
 
 func _on_ground_hit():
 	$Bird.falling = false
